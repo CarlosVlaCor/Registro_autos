2b232b402d7c4305a18bb68450fc3c37ae509178
Carlos E. Valenzuela C <89716956+CarlosVlaCor@users.noreply.github.com>
2023-03-08 12:27:13 -0700

    cambiado nombre de archivo para borrar y agregado encript

--- insertar_usuario.php
+++ insertar_usuario.php
@@ -5,12 +5,15 @@ $marca = $_POST["marca"];
 $modelo = $_POST["modelo"];
 $version = $_POST["version"];
 $serie = $_POST["serie"];
+$pws = $_POST["pws"];
 $con = mysqli_connect('localhost','carlos','12345');
  if (!$con)
  die('No se pudo conectar: ' . mysqli_error($con));
+
+ $pass_cifrado = password_hash($pws, PASSWORD_DEFAULT);
  mysqli_select_db($con,'registro_autos');
- $sql="insert into vehiculo(placa, tipo_vehiculo, marca, modelo, version_modelo, num_serie)";
- $sql=$sql." values('".$placa."', '".$tipo."','".$marca."','".$modelo."','".$version."','".$serie."')";
+ $sql="insert into vehiculo(placa, tipo_vehiculo, marca, modelo, version_modelo, num_serie, pws)";
+ $sql=$sql." values('".$placa."', '".$tipo."','".$marca."','".$modelo."','".$version."','".$serie."', '".$pass_cifrado."')";
  $result = mysqli_query($con,$sql);
  mysqli_close($con);
 ?>
